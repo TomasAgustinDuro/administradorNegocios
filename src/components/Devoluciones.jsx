3e63dd87d1916eafd637417e50929754eeb899a8
Tomas <durotomaas@gmail.com>
2024-08-23 20:02:18 -0300

    tickets is styled

--- src/components/Devoluciones.jsx
+++ src/components/Devoluciones.jsx
@@ -29,41 +29,40 @@ export function Devoluciones() {
       .catch((error) => {
         console.error(error.message);
       });
-  },[shouldRefresh]);
+  }, [shouldRefresh]);
 
   function uploadTicketReturns(formMethod, formData) {
     const url = "http://localhost:8000/diarios/api/devoluciones/";
 
     fetch(url, {
       method: formMethod,
-      body: formData
+      body: formData,
     })
       .then((response) => {
         if (!response.ok) {
           throw new Error(`Response status: ${response.status}`);
         }
-        setShouldRefresh((prev) => !prev)
+        setShouldRefresh((prev) => !prev);
       })
       .catch((error) => {
         console.error(error.message);
       });
   }
 
-  function deleteTicketReturns(ticketId){
-    const url = `http://localhost:8000/diarios/api/devoluciones/${ticketId}/`
+  function deleteTicketReturns(ticketId) {
+    const url = `http://localhost:8000/diarios/api/devoluciones/${ticketId}/`;
 
     fetch(url, {
       method: "DELETE",
     })
       .then((response) => {
         if (response.ok) {
-          console.log("Devolucion eliminada con exito")
-          setDevoluciones(devoluciones.filter((item) => item.id !== ticketId))
-          
+          console.log("Devolucion eliminada con exito");
+          setDevoluciones(devoluciones.filter((item) => item.id !== ticketId));
         } else {
-          console.error("Error al eliminar articulo")
+          console.error("Error al eliminar articulo");
         }
-        setShouldRefresh((prev) => !prev)
+        setShouldRefresh((prev) => !prev);
       })
       .catch((error) => {
         console.error(error.message);
@@ -72,25 +71,34 @@ export function Devoluciones() {
 
   const mostrarBoletas = () => {
     const baseUrl = "http://localhost:8000";
-  
-    return devoluciones.map((devolucion) => (
-      <div key={devolucion.id} className="boleta">
-        <a href={`${baseUrl}${devolucion.url}`} target="_blank" rel="noopener noreferrer">
-          <img src={`${baseUrl}${devolucion.url}`} alt={`Imagen ${devolucion.id}`} />
-        </a>
-        <p>{devolucion.fecha}</p>
 
-        <button
-          onClick={() => {
-            deleteTicketReturns(devolucion.id)
-          }}
+    return devoluciones.map((devolucion) => (
+      <div key={devolucion.id} className="ticket">
+        <a
+          href={`${baseUrl}${devolucion.url}`}
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          Borrar
-        </button>
+          <img
+            src={`${baseUrl}${devolucion.url}`}
+            alt={`Imagen ${devolucion.id}`}
+          />
+        </a>
+        <div className="footer-ticket">
+          <p>{devolucion.fecha}</p>
+
+          <button
+            className="button"
+            onClick={() => {
+              deleteTicketReturns(devolucion.id);
+            }}
+          >
+            Borrar
+          </button>
+        </div>
       </div>
     ));
   };
-  
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -99,30 +107,62 @@ export function Devoluciones() {
     const formData = new FormData(form);
 
     uploadTicketReturns(form.method, formData);
-
   }
 
   return (
     <>
       <section className="section-upload-ticket">
-        👩
-        <form method="POST" encType="multipart/form-data" onSubmit={handleSubmit}>
-          <input
-            type="file"
-            accept="image/*"
-            name="imagen"
-            placeholder="Carga tu imagen aca"
-          />
-          <input type="date" name="fecha" id="" placeholder="¿Que dia es hoy?" />
+        <form
+          method="POST"
+          encType="multipart/form-data"
+          onSubmit={handleSubmit}
+        >
+          <label htmlFor="file" className="custom-file-upload">
+            <div className="icon">
+              <svg
+                viewBox="0 0 24 24"
+                fill=""
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z"
+                    fill=""
+                  ></path>{" "}
+                </g>
+              </svg>
+            </div>
+            <div className="text">
+              <span>Click to upload image</span>
+            </div>
+            <input id="file" type="file" accept="image/*" name="imagen" />
+          </label>
 
-          <button>
-            Cargar imagen
-          </button>
+          <input
+            type="date"
+            name="fecha"
+            id=""
+            className="ticket-date"
+            placeholder="¿Que dia es hoy?"
+          />
+
+          <button className="button button-upload-image">Cargar imagen</button>
         </form>
       </section>
 
-      <section className="contenedor-boletas">
-        <div className="boletas">{mostrarBoletas()}</div>
+      <section className="tickets-container">
+       {
+        devoluciones.length > 0 ? ( <div className="tickets">{mostrarBoletas()}</div>) : <h3>No hay boletas cargadas</h3>
+       }
       </section>
     </>
   );
